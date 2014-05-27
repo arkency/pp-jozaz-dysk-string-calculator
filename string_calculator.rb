482d885ca0625ebf2b2d19f4ca742e65001a1ffa
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2014-05-27 19:40:57 +0200

    refactored add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,22 +6,42 @@ class StringCalculator
   end
 
   def add(expression)
-    delimiter = ','
-    if custom_delimiter?(expression)
-      delimiter = expression[2]
-      expression = expression[4..-1]
-    end
-    numbers = expression.split(/(#{delimiter}|\n)/).map {|x| x.to_i}
+    expression, delimiter = set_expression_and_delimiter(expression)
+    numbers = get_numbers(expression, delimiter)
+    check_for_negatives(numbers)
+    sum(numbers)
+  end
 
+  private
+
+  def custom_delimiter?(expression)
+    expression.start_with?('//')
+  end
+
+  def get_numbers(expression, delimiter)
+    expression.split(/(#{delimiter}|\n)/).map {|x| x.to_i}
+  end
+
+  def check_for_negatives(numbers)
     negatives = numbers.select {|element| element < 0}
     if negatives.any?
       raise NegativeNumberError.new("negatives not allowed: #{negatives.join(', ')}")
     end
-
-    numbers.inject(0, :+)
   end
 
-  def custom_delimiter?(expression)
-    expression.start_with?('//')
+  def set_expression_and_delimiter(expression)
+    if custom_delimiter?(expression)
+      delimiter  = expression[2]
+      expression = expression[4..-1]
+    else
+      delimiter  = ','
+      expression = expression
+    end
+
+    return expression, delimiter
+  end
+
+  def sum(numbers)
+    numbers.inject(0, :+)
   end
 end
